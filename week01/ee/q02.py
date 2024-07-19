ca4eb3da5b19e9f25328a57629625e8777ddefdd
ebeleey <ebeleey@gmail.com>
2024-07-19 17:28:22 +0900

    ee: #2 곱하기 혹은 더하기

--- week01/ee/q02.py
+++ week01/ee/q02.py
@@ -3,9 +3,9 @@
 chars = input() #문자열 입력받기
 result = 1
 for char in chars:
-    if char == '0' or char == '1': # 0이면 패스
+    if char == '0' or char == '1': # 1이면 더하기
         result += int(char)
-    else: #숫자면 그냥 다 곱하기
+    else: #이외에는 그냥 다 곱하기
         result *= int(char)
 print(result)
 
