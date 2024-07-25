67ddc42484e7cbfe4a4109040db9e1253e1dfb5d
kimpepe <unboxing96@gmail.com>
2024-07-25 10:33:30 +0900

    fix : 07_럭키스트레이트 파일명 변경

--- week02/kth/07_럭키스트레이트.py
+++ week02/kth/07_럭키스트레이트.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open("01_럭키스트레이트.txt", "r")
+sys.stdin = open("07_럭키스트레이트.txt", "r")
 
-score = [int(elem) for elem in input()]
+score = list(map(int, input()))
 print("LUCKY" if sum(score[:len(score) // 2]) == sum(score[len(score) // 2:]) else "READY")
