343ac05b229e45af389884b2396994ac57d9c700
mins-git <m2insapp@gmail.com>
2024-10-18 22:29:12 +0900

    feat: java Algo day1

--- week10/hdm/src/day1/약수구하기_2501/약수구하기.java
+++ week10/hdm/src/day1/약수구하기_2501/약수구하기.java
@@ -3,15 +3,16 @@ package day1.약수구하기_2501;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class 약수구하기 {
     public static void main(String[] args) throws IOException {
 
-        String filePath = "src/day1/약수구하기_2501/input.txt";
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String filePath = "hdm/src/day1/약수구하기_2501/input.txt";
+//        BufferedReader br = new BufferedReader(new FileReader(filePath));
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
         String[] values = br.readLine().split(" ");
@@ -25,7 +26,13 @@ public class 약수구하기 {
                 resultList.add(i);
             }
         }
-        System.out.println(resultList.get(b-1));
+
+        if (resultList.size() >= b){
+            System.out.println(resultList.get(b-1));
+        } else {
+            System.out.println(0);
+        }
+
 //        System.out.println(resultList.get(b));
 
         /*
