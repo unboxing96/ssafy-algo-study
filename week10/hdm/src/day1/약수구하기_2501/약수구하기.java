c0cbcc37d39fdb7ef3a8cf92afa791cd1fa1d272
min <m2insapp@gmail.com>
2024-10-18 17:57:53 +0900

    feat:week10 약수구하기

---
+++ week10/hdm/src/day1/약수구하기_2501/약수구하기.java
@@ -0,0 +1,36 @@
+package day1.약수구하기_2501;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 약수구하기 {
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "src/day1/약수구하기_2501/input.txt";
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String[] values = br.readLine().split(" ");
+        int a = Integer.parseInt(values[0]), b = Integer.parseInt(values[1]);
+
+        br.close();
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+        for (int i = 1; i <= a; i++){
+            if(a % i == 0){
+                resultList.add(i);
+            }
+        }
+        System.out.println(resultList.get(b-1));
+//        System.out.println(resultList.get(b));
+
+        /*
+        values 0번째의 약수를 구한 후 1번째의 값을 출력하면됨.
+         */
+
+    }
+}
